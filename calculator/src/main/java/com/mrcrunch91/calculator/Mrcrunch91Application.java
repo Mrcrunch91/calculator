59bf4b7951553511b975592b7c0bf0b20e36fe95
BigNateLinuxDesktop <bignatelinuxdesktop@BigNateDeskTop.attlocal.net>
2023-11-01 03:41:11 -0400

    Update for springboot with java 11 environment

---
+++ calculator/src/main/java/com/mrcrunch91/calculator/Mrcrunch91Application.java
@@ -0,0 +1,13 @@
+package com.mrcrunch91.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mrcrunch91Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mrcrunch91Application.class, args);
+	}
+
+}
