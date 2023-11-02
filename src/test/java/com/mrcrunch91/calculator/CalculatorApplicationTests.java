99fdaee128436c2f9d0dd9e992513ed2d2bdcdf0
BigNateLinuxDesktop <bignatelinuxdesktop@BigNateDeskTop.attlocal.net>
2023-11-02 02:49:50 -0400

    Switched to MAVEN

--- src/test/java/com/mrcrunch91/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/mrcrunch91/calculator/CalculatorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mrcrunch91ApplicationTests {
+class CalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
