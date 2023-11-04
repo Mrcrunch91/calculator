c1b37b55e018067df41feb622bcb8d2f4708b23e
Mrcrunch91 <alterac91@yahoo.com>
2023-11-04 05:32:38 -0400

    Reverted main class

---
+++ src/main/java/com/mrcrunch91/calculator/HelloController.java
@@ -0,0 +1,16 @@
+package com.mrcrunch91.calculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller
+// @ResponseBody
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello NateDogg, Spring Boot Here!";
+    }
+    
+}
