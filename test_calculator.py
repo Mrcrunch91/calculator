ac15f45e936c113f2a1cb86c7641e6df8c7d0b4a
Mrcrunch91 <alterac91@yahoo.com>
2023-11-09 23:22:18 -0500

    Failure works, change back to be successful

--- test_calculator.py
+++ test_calculator.py
@@ -5,5 +5,5 @@ from calculator import *
 class TestCalculator(unittest.TestCase):
     def test_multiply(self):
         nums = [1,2,3,4,5]
-        self.assertEqual(multiplyAllNumbers(nums), 10)
+        self.assertEqual(multiplyAllNumbers(nums), 120)
 
